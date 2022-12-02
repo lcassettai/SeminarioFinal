43a6890822ab961731c5c3ec877d2c7c19f4aec2
Luciano Cassettai <lcassettai>
2022-12-02 17:27:14 -0300

    ADD tailwindcss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Seminario Final</h1>
+      <h1 className="text-3xl font-bold underline">
+        Seminario final
+      </h1>
     </div>
   );
 }
