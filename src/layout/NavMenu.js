8cee789e81df6ae354f19c13aa3d9722cba7f4b0
lcassettai <luciano.cassettai@gmail.com>
2022-12-09 00:22:04 -0300

    Fix mueve el layout fuera de la vista

--- src/layout/NavMenu.js
+++ src/layout/NavMenu.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
-import Logo from '../../assets/img/logo.svg';
+import Logo from '../assets/img/logo.svg';
 import { Link } from 'react-router-dom';
 
 
--- src/views/menuHome/MenuHome.js
+++ src/views/menuHome/MenuHome.js
@@ -1,5 +1,5 @@
 import {Link} from 'react-router-dom';
-import NavMenu from '../layout/NavMenu';
+import NavMenu from '../../layout/NavMenu';
 import './menuHome.css';
 import { TbHandClick } from "react-icons/tb";
 import { BiQrScan, BiSearchAlt} from "react-icons/bi";
