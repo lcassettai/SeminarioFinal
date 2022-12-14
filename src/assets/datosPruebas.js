33741caece62949b3c0950419fbf18edaaa5c580
Luciano Cassettai <lcassettai>
2022-12-14 18:30:43 -0300

    agrega mesas a los datos de prueba

--- src/assets/datosPruebas.js
+++ src/assets/datosPruebas.js
@@ -4,35 +4,105 @@ const listadoInicialRestaurantes = [
         nombre: "El Tano",
         estrellas: 4,
         descripcion: "Vestibulum interdum feugiat neque, eu dignissim sapien suscipit in. Curabitur mollis lobortis mauris ut eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam molestie egestas pharetra. Curabitur vitae pharetra leo. Ut est felis, aliquet et ullamcorper vitae, tempus a elit. Donec quis ipsum sem.",
-        imagen: 'demo-restaurante-1.jpg'
+        imagen: 'demo-restaurante-1.jpg',
+        mesas : [
+            {
+                idMesa : 1,
+                codigo : 'AB1A'
+            },
+            {
+                idMesa : 2,
+                codigo : 'AB2A'
+            },
+            {
+                idMesa : 3,
+                codigo : 'AB3A'
+            }
+        ]
     },
     {
         id:2,
         nombre: "El Rancho",
         estrellas:5,
         descripcion: "Vestibulum interdum feugiat neque, eu dignissim sapien suscipit in. Curabitur mollis lobortis mauris ut eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam molestie egestas pharetra. Curabitur vitae pharetra leo. Ut est felis, aliquet et ullamcorper vitae, tempus a elit. Donec quis ipsum sem.",
-        imagen: 'demo-restaurante-2.jpeg'
+        imagen: 'demo-restaurante-2.jpeg',
+        mesas : [
+            {
+                idMesa : 4,
+                codigo : 'AC1A'
+            },
+            {
+                idMesa : 5,
+                codigo : 'AC2A'
+            },
+            {
+                idMesa : 6,
+                codigo : 'AC3A'
+            }
+        ]
     },
     {
         id:3,
         nombre: "Los Pinos",
         estrellas: 4,
         descripcion: "Vestibulum interdum feugiat neque, eu dignissim sapien suscipit in. Curabitur mollis lobortis mauris ut eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam molestie egestas pharetra. Curabitur vitae pharetra leo. Ut est felis, aliquet et ullamcorper vitae, tempus a elit. Donec quis ipsum sem.",
-        imagen: 'demo-restaurante-3.jpeg'
+        imagen: 'demo-restaurante-3.jpeg',
+        mesas : [
+            {
+                idMesa : 7,
+                codigo : 'AD1A'
+            },
+            {
+                idMesa : 8,
+                codigo : 'AD2A'
+            },
+            {
+                idMesa : 9,
+                codigo : 'AD3A'
+            }
+        ]
     },
     {
         id:4,
         nombre: "La Rueda",
         estrellas: 3,
         descripcion: "Vestibulum interdum feugiat neque, eu dignissim sapien suscipit in. Curabitur mollis lobortis mauris ut eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam molestie egestas pharetra. Curabitur vitae pharetra leo. Ut est felis, aliquet et ullamcorper vitae, tempus a elit. Donec quis ipsum sem.",
-        imagen: 'demo-restaurante-4.jpeg'
+        imagen: 'demo-restaurante-4.jpeg',
+        mesas : [
+            {
+                idMesa : 10,
+                codigo : 'AE1A'
+            },
+            {
+                idMesa : 11,
+                codigo : 'AE2A'
+            },
+            {
+                idMesa : 12,
+                codigo : 'AE3A'
+            }
+        ]
     },
     {
         id:5,
         nombre: "Mendieta",
         estrellas: 3,
         descripcion: "Vestibulum interdum feugiat neque, eu dignissim sapien suscipit in. Curabitur mollis lobortis mauris ut eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam molestie egestas pharetra. Curabitur vitae pharetra leo. Ut est felis, aliquet et ullamcorper vitae, tempus a elit. Donec quis ipsum sem.",
-        imagen: 'demo-restaurante-5.jpeg'
+        imagen: 'demo-restaurante-5.jpeg',
+        mesas : [
+            {
+                idMesa : 13,
+                codigo : 'AF1A'
+            },
+            {
+                idMesa : 14,
+                codigo : 'AF2A'
+            },
+            {
+                idMesa : 15,
+                codigo : 'Af3A'
+            }
+        ]
     }
 ];
 
