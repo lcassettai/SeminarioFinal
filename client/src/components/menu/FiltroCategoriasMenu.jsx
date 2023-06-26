a641d3c06ce7b1f257cb4c8e41480cf04b0ba223
lcassettai <luciano.cassettai@gmail.com>
2023-06-25 22:54:24 -0300

    ADD categorias desde la BD

--- client/src/components/menu/FiltroCategoriasMenu.jsx
+++ client/src/components/menu/FiltroCategoriasMenu.jsx
@@ -5,9 +5,31 @@ import {
   FaGlassCheers,
   FaIceCream,
   FaFish,
+  FaSandwich,
 } from "react-icons/fa";
+import { GiSandwich } from "react-icons/gi"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {getCategoriasSucursal} from '../../api/categorias';
 
 const FiltroCategoriasMenu = () => {
+  const [categorias,setCategorias] = useState();
+  const { idRestaurante } = useParams();
+
+  useEffect(() => {
+    const getCategorias = async () => {
+      try {
+        const result = await getCategoriasSucursal(idRestaurante);
+        
+        setCategorias(await result.json());
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    
+    getCategorias();
+  },[]);
+
     const handleSelected = (elemento) => {
         console.log(elemento);
     }
@@ -16,21 +38,27 @@ const FiltroCategoriasMenu = () => {
     <>
       <div>
         <div className="w-full grid grid-flow-col auto-cols-max overflow-x-auto container-snap 	">
-          <FiltroMenuCategoria text="Hamburguesas" identificador="1" setOnSelected={handleSelected}>
-            <FaHamburger />
-          </FiltroMenuCategoria>
-          <FiltroMenuCategoria text="Bebidas" identificador="2" setOnSelected={handleSelected}>
-            <FaGlassCheers />
-          </FiltroMenuCategoria>
-          <FiltroMenuCategoria text="Pizzas"  identificador="3" setOnSelected={handleSelected}>
-            <FaPizzaSlice />
-          </FiltroMenuCategoria>
-          <FiltroMenuCategoria text="Postres"  identificador="4" setOnSelected={handleSelected}>
-            <FaIceCream />
-          </FiltroMenuCategoria>
-          <FiltroMenuCategoria text="Pescados"  identificador="5" setOnSelected={handleSelected}>
-            <FaFish />
-          </FiltroMenuCategoria>
+          {
+            categorias ? categorias.map( (c) => {
+            let imagen = "";
+            switch (c.categoria.toLowerCase()) {
+              case "hamburguesas":
+                imagen = <FaHamburger />
+                break;
+              case "pizzas":
+                  imagen = <FaPizzaSlice />
+                  break;
+              case "lomitos":
+                imagen = <GiSandwich />
+                break;
+              default:
+                break;
+            }
+            return (<FiltroMenuCategoria key={c.id_categoria} text={c.categoria}  identificador={c.id_categoria} setOnSelected={handleSelected}>
+                {imagen}
+            </FiltroMenuCategoria>)
+            }) : ""
+          }
         </div>
       </div>
     </>
