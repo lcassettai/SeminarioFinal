9e2615c679f1e41d9481a71622cc49a97914a725
Luciano Cassettai <lcassettai>
2023-07-02 18:14:31 -0300

    ADD id_cliente en el token

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -12,19 +12,21 @@ const login = async (req,res) => {
     }
 
     const usuarioLogin =  await loginModel.login(usuario,clave);
-
-    if (usuarioLogin !== undefined && usuarioLogin.length > 0) {
-
-      const sesionUsuario ={
-        "id_usuario" : usuarioLogin[0].id_persona,
-        "usuario": usuarioLogin[0].usuario
-      }
-
-      const token = jwt.sign({sesionUsuario}, secretKey, { expiresIn: '24h' });
-      res.json({ token });
-    } else {
-      res.status(401).json({ error: 'Credenciales inválidas' });
+    
+    //Validamos el usuario
+    if (!usuarioLogin || usuarioLogin.length <= 0) {
+       res.status(401).json({ error: 'Credenciales inválidas' });
+       return;
     }
+
+    const sesionUsuario ={
+      "id_usuario" : usuarioLogin[0].id_persona,
+      "id_cliente": usuarioLogin[0].id_cliente,
+      "usuario": usuarioLogin[0].usuario
+    }
+
+    const token = jwt.sign({sesionUsuario}, secretKey, { expiresIn: '24h' });
+    res.json({ token });    
 }
 
 module.exports = {
--- server/models/loginModel.js
+++ server/models/loginModel.js
@@ -1,7 +1,10 @@
 const db = require('../services/db');
 
 const login = async (usuario,clave) => {
-    const resultado = await db.query("SELECT * FROM personas WHERE usuario = $1 AND clave = $2",[usuario,clave]);
+    const resultado = await db.query(`SELECT * 
+            FROM personas p inner join clientes c
+            ON p.id_persona = c.id_persona
+            WHERE usuario = $1 AND clave = $2 AND bloqueado = false`,[usuario,clave]);
     return resultado.rows;
 }
 
